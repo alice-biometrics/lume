b20a4860fea0df040a544eda3db5de4099d8554e
jlamoso <jlamoso@gradiant.org>
2020-01-23 16:35:18 +0100

    Added SetupService and helpers

---
+++ lume/src/infrastructure/services/setup/setup_item_file.py
@@ -0,0 +1,48 @@
+import os
+from zipfile import BadZipFile
+
+import requests
+from requests.auth import HTTPBasicAuth
+from meiga import Result, Success, Failure
+
+from lume.config import DependencyConfig
+from lume.src.domain.services.interface_logger import ILogger, INFO
+from lume.src.infrastructure.services.setup.setup_errors import CrendentialsEnvError, BadZipFileError
+from lume.src.infrastructure.services.setup.setup_item import SetupItem
+from lume.src.infrastructure.services.setup.setup_utils import unzip_file
+
+
+class SetupItemFile(SetupItem):
+
+    def run(self, name: str, dependency_config: DependencyConfig, logger: ILogger) -> Result:
+        dependency_path = os.path.join(self.base_path, name)
+        if os.path.exists(dependency_path):
+            logger.log(INFO, f"{self.__class__.__name__} - dependency {name} already exists")
+            return Success()
+        os.makedirs(dependency_path)
+
+        if not dependency_config.auth_required:
+            auth = None
+        else:
+            credentials_var = os.environ.get(dependency_config.credentials_env)
+            if credentials_var is None:
+                return Failure(CrendentialsEnvError(dependency_config.credentials_env))
+            username, password = credentials_var.split(":")
+            auth = HTTPBasicAuth(username, password)
+
+        self.__download_file(dependency_path, dependency_config.url, auth)
+
+        if dependency_config.unzip:
+            try:
+                unzip_file(dependency_path, dependency_config.url)
+            except BadZipFile:
+                return Failure(BadZipFileError(name))
+
+        return Success()
+
+    @staticmethod
+    def __download_file(dst: str, url: str, auth: HTTPBasicAuth = None):
+        r = requests.get(url, auth=auth, stream=True)
+        dst_filename = os.path.split(url)[-1]
+        with open(os.path.join(dst, dst_filename), 'wb') as f:
+            f.write(r.content)
---
+++ lume/src/infrastructure/services/setup/setup_utils.py
@@ -0,0 +1,31 @@
+import os
+import zipfile
+import shutil
+
+
+def unzip_file(dst: str, url: str):
+    file_name = url.split('/')[-1]
+    path_zipfile = os.path.join(dst, file_name)
+    zip_ref = zipfile.ZipFile(path_zipfile, 'r')
+    zip_ref.extractall(dst)
+    zip_ref.close()
+    os.remove(path_zipfile)
+
+    if contain_one_folder(dst):
+        content = os.listdir(dst)
+        name_path = content[0]
+        move_content(os.path.join(dst, name_path), dst)
+        shutil.rmtree(os.path.join(dst, name_path))
+
+
+def contain_one_folder(path):
+    content = os.listdir(path)
+    if len(content) == 1 and os.path.isdir(os.path.join(path, content[0])):
+        return True
+    return False
+
+
+def move_content(unzip_directory_path, dst):
+    unzip_directory_path_content = os.listdir(unzip_directory_path)
+    for item_name in unzip_directory_path_content:
+        shutil.move(os.path.join(unzip_directory_path, item_name), dst)
