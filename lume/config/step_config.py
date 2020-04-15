be7837c08493dda9cde863991ff2d7b6c7be8129
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-04-15 16:05:14 +0200

    Add setup and teardown feature in steps

--- lume/config/step_config.py
+++ lume/config/step_config.py
@@ -7,22 +7,37 @@ class StepConfig:
     run: List[str]
     cwd: Optional[str] = None
     envs: Optional[Dict[str, str]] = None
+    setup: List[str] = None
+    teardown: List[str] = None
 
     @staticmethod
     def from_dict(kdict):
-        krun = kdict.get("run")
-        if not krun:
-            raise TypeError("StepConfig must contains run variable")
-        if isinstance(krun, str):
-            run = [krun]
-        elif isinstance(krun, list):
-            run = krun
+        run = StepConfig.check_list_or_str_item(kdict, "run", required=True)
+        setup = StepConfig.check_list_or_str_item(kdict, "setup", required=False)
+        teardown = StepConfig.check_list_or_str_item(kdict, "teardown", required=False)
+        return StepConfig(
+            run=run,
+            cwd=kdict.get("cwd"),
+            envs=kdict.get("envs"),
+            setup=setup,
+            teardown=teardown,
+        )
+
+    @staticmethod
+    def check_list_or_str_item(kdict, key, required=False):
+        kvalue = kdict.get(key)
+        if not kvalue and required:
+            raise TypeError(f"StepConfig must contains {key} variable")
+
+        if isinstance(kvalue, str):
+            value = [kvalue]
+        elif isinstance(kvalue, list):
+            value = kvalue
         else:
-            raise TypeError(
-                "StepConfig must contains run variable (Only list and str is supported)"
-            )
-
-        return StepConfig(run=run, cwd=kdict.get("cwd"), envs=kdict.get("envs"))
-
-    # setup: List[str] = None
-    # teardown: List[str] = None
+            if required:
+                raise TypeError(
+                    f"StepConfig must contains {key} variable (Only list and str is supported)"
+                )
+            else:
+                value = None
+        return value
