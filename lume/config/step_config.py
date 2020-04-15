2f750ce2da8531d0fa7c60c902d5c29b68a10875
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-04-15 14:26:56 +0200

    Now is possible to config one-line run in steps

--- lume/config/step_config.py
+++ lume/config/step_config.py
@@ -1,11 +1,28 @@
 from dataclasses import dataclass
-from dataclasses_json import dataclass_json
 from typing import List, Optional, Dict
 
 
-@dataclass_json
 @dataclass
 class StepConfig:
     run: List[str]
     cwd: Optional[str] = None
     envs: Optional[Dict[str, str]] = None
+
+    @staticmethod
+    def from_dict(kdict):
+        krun = kdict.get("run")
+        if not krun:
+            raise TypeError("StepConfig must contains run variable")
+        if isinstance(krun, str):
+            run = [krun]
+        elif isinstance(krun, list):
+            run = krun
+        else:
+            raise TypeError(
+                "StepConfig must contains run variable (Only list and str is supported)"
+            )
+
+        return StepConfig(run=run, cwd=kdict.get("cwd"), envs=kdict.get("envs"))
+
+    # setup: List[str] = None
+    # teardown: List[str] = None
