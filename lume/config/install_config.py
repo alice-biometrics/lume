a158b025371a704ec6bb5452e0498cb3931659f3
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-07-23 17:58:40 +0200

    Add os command specific values on install static command

--- lume/config/install_config.py
+++ lume/config/install_config.py
@@ -1,6 +1,31 @@
 from dataclasses import dataclass
 from typing import List, Optional
 from lume.config.check_list_or_str_item import check_list_or_str_item
+from sys import platform
+
+
+def get_platform():
+    if platform == "linux" or platform == "linux2":
+        return "linux"
+    elif platform == "darwin":
+        return "macos"
+    elif platform == "win32":
+        return "windows"
+
+
+def check_os_list_or_str_item(kdict, required=False, suffix=""):
+    kvalue = kdict.get("run")
+    if not kvalue and required:
+        raise TypeError(f"StepConfig must contains run {suffix} variable")
+
+    if isinstance(kvalue, dict):
+        platform = get_platform()
+        platform_commands = check_list_or_str_item(kvalue, platform)
+        all_commands = check_list_or_str_item(kvalue, "all")
+        value = platform_commands + all_commands
+    else:
+        value = check_list_or_str_item(kdict, "run")
+    return value
 
 
 @dataclass
@@ -10,6 +35,7 @@ class InstallConfig:
 
     @staticmethod
     def from_dict(kdict):
-        run = check_list_or_str_item(kdict, "run")
+
+        run = check_os_list_or_str_item(kdict, "run")
 
         return InstallConfig(run=run if run else [], cwd=kdict.get("cwd"))
