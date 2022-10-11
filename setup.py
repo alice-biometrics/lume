e6fdc02a23ea1107c302b3025da16c600a6d059b
acostapazo <acosta@alicebiometrics.com>
2022-10-11 17:00:20 +0200

    chore(python-version): add python 3.11 support check

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        # "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=setuptools.find_packages(),
     include_package_data=True,
